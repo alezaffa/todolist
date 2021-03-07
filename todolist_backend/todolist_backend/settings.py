13f8ad9dfb12283ab9d54fe7836f3466daef2f36
Alessandro Zaffaroni <a.zaffaroni@reply.it>
2021-03-07 11:47:19 +0100

    CRUD todolist application api

--- todolist_backend/todolist_backend/settings.py
+++ todolist_backend/todolist_backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'todolist',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'todolist_backend.urls'
@@ -69,6 +73,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'todolist_backend.wsgi.application'
 
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
