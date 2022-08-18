c6a27cfef91fb9203668fb6d4f0fe46891f413c9
Alessandro Zaffaroni <a.zaffaroni@reply.it>
2022-08-18 22:56:41 +0200

    Project update
    Fixed some issue after the project brush up

--- todolist_backend/todolist_backend/urls.py
+++ todolist_backend/todolist_backend/urls.py
@@ -1,7 +1,7 @@
 """todolist_backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -22,6 +22,7 @@ router = routers.DefaultRouter()
 router.register(r'todos', views.TodoView, 'todo')
 
 urlpatterns = [
+    path('', admin.site.urls),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
